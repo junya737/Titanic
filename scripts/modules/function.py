3db1230de7162333263b7712ebb67e99da63518b
ihiratch <junya737@gmail.com>
2023-05-24 15:55:02 -0700

    Organize codes

--- scripts/modules/function.py
+++ scripts/modules/function.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import datetime
 
 
 def add_one_hot(df, column_name):
@@ -22,3 +23,14 @@ def add_one_hot(df, column_name):
 def nan_to_mean(df):
     df = df.fillna(df.mean())
     return df
+
+
+def save_kaggle_prediction(prediction, Id, target_col_name="target",
+                           Id_col_name="Id"):
+    # my_prediction(予測データ）とPassengerIdをデータフレームへ落とし込む
+    my_solution = pd.DataFrame(prediction, Id, columns=[target_col_name])
+    dt_now = datetime.datetime.now()
+    # my_tree_one.csvとして書き出し
+    my_solution.to_csv("../data/result/prediction_" +
+                       dt_now.strftime('%Y%m%d_%H%M%S') + ".csv",
+                       index_label=[Id_col_name])
