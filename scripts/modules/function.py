21ee7edee6db4d8a2c80b80e6d98f65b28a0b5b5
ihiratch <junya737@gmail.com>
2023-05-24 16:18:28 -0700

    Make get_score StratifiedKFold_cv

--- scripts/modules/function.py
+++ scripts/modules/function.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import datetime
+from sklearn.model_selection import StratifiedGroupKFold
+from sklearn.model_selection import cross_validate
 
 
 def add_one_hot(df, column_name):
@@ -34,3 +37,24 @@ def save_kaggle_prediction(prediction, Id, target_col_name="target",
     my_solution.to_csv("../data/result/prediction_" +
                        dt_now.strftime('%Y%m%d_%H%M%S') + ".csv",
                        index_label=[Id_col_name])
+
+
+def get_score_StratifiedKFold_cv(clf, x, y, n_splits, scoring, shuffle=True):
+    """層化抽出KfoldCVのスコアを取得
+
+    Args:
+        clf (class): model
+        x (np_array): 
+        y (np_array): 
+        n_splits (int): 
+        scoring (str): 
+        shuffle (bool): Defaults to True.
+
+    Returns:
+        float: score
+    """
+    kf = StratifiedGroupKFold(
+        n_splits=n_splits, shuffle=shuffle, random_state=42)
+    result = cross_validate(clf, x, y, cv=kf, scoring=scoring)
+
+    return np.mean(result['test_score'])
