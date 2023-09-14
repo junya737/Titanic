4f7247777d29526c2fbff83533648684fba72b68
ihiratch <junya737@gmail.com>
2023-09-14 16:14:29 -0700

    Preprocess again

--- scripts/modules/function.py
+++ scripts/modules/function.py
@@ -18,8 +18,8 @@ def add_one_hot(df, column_name):
         _type_: df
     """
     if (column_name in df.columns):
-        one_hot_sex = pd.get_dummies(df[column_name], prefix=column_name)
-        df = pd.concat([df, one_hot_sex], axis=1)
+        one_hot = pd.get_dummies(df[column_name], prefix=column_name)
+        df = pd.concat([df, one_hot], axis=1)
         df.pop(column_name)
         if (column_name == "Sex"):
             df.pop("Sex_male")
